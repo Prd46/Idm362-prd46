8422701df7b4297372ea1517c92a48d287580e96
Prd46 <prd46@drexel.edu>
2023-02-23 19:23:52 -0500

    Functional chords

--- idm362-prd46/ViewControllerFrets.swift
+++ idm362-prd46/ViewControllerFrets.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewControllerFrets: UIViewController {
 
@@ -15,24 +16,38 @@ class ViewControllerFrets: UIViewController {
     
     @IBOutlet weak var typeMenuButton: UIButton!
     
+    var keyMenuValue = "C"
+    var typeMenuValue = "Major"
+    var myAudioPlayerObj = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpPopUpButton()
         setUpPopUpButton2()
         // Do any additional setup after loading the view.
-        
-        
+    }
+    
         func setUpPopUpButton(){
+    
             //build setup popup button values
             let optionsObj = {
                 //(action : UIAction) in print(action.title)
-                (action : UIAction) in self.fretsImage.image = UIImage(named:"\(action.title)")
-                print("\(action.title)")
+                
+                (action : UIAction)
+                in self.fretsImage.image = UIImage(named:"\(action.title) \(self.typeMenuValue)")
+                print("\(action.title) \(self.typeMenuValue)")
+                self.keyMenuValue = action.title
+                
                 //self.displayLabel.text = "\(action.title)"
             }
             keyMenuButton.menu = UIMenu(children : [
                 UIAction(title: "C", state: .on, handler: optionsObj),
-                UIAction(title: "E", handler: optionsObj)
+                UIAction(title: "D", handler: optionsObj),
+                UIAction(title: "E", handler: optionsObj),
+                UIAction(title: "F", handler: optionsObj),
+                UIAction(title: "G", handler: optionsObj),
+                UIAction(title: "A", handler: optionsObj),
+                UIAction(title: "B", handler: optionsObj)
                 
             ])
             keyMenuButton.showsMenuAsPrimaryAction = true
@@ -44,25 +59,38 @@ class ViewControllerFrets: UIViewController {
             //build setup popup button values
             let optionsObj2 = {
                 //(action : UIAction) in print(action.title)
-                (action : UIAction) in self.fretsImage.image = UIImage(named:"\(action.title)")
-                print(action.title)
+                (action : UIAction) in self.fretsImage.image = UIImage(named:"\(self.keyMenuValue) \(action.title)")
+                print("\(self.keyMenuValue) \(action.title)")
+                self.typeMenuValue = action.title
                 //self.displayLabel.text = "\(action.title)"
             }
             typeMenuButton.menu = UIMenu(children : [
-                UIAction(title: "Minor", state: .on, handler: optionsObj2),
-                UIAction(title: "Major", handler: optionsObj2),
+                UIAction(title: "Major", state: .on, handler: optionsObj2),
+                UIAction(title: "Minor", handler: optionsObj2),
                 
             ])
             typeMenuButton.showsMenuAsPrimaryAction = true
             typeMenuButton.changesSelectionAsPrimaryAction = true
  
-        }
+   
         
         
 
     }
     
-
+    @IBAction func playButton(_ sender: Any) {
+        let guitarSound = Bundle.main.path(forResource: "chords/\(self.keyMenuValue) \(self.typeMenuValue)", ofType: "mp3")
+        do {
+            myAudioPlayerObj = try
+            AVAudioPlayer(contentsOf: URL(fileURLWithPath: guitarSound!))
+            myAudioPlayerObj.prepareToPlay()
+           // print("sound file is ready")
+        } catch {
+            print(error)
+        }
+        myAudioPlayerObj.play()
+    }
+    
     /*
     // MARK: - Navigation
 
